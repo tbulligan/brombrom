561914a73d2025ae4d6db2ae0b9b6aea4df87924
Tomaso Bulligan <tomaso.bulligan@gmail.com>
2026-04-23 11:59:12 +0200

    fix(app): fix build method syntax errors in main.dart

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -773,11 +773,13 @@ class _InstallerScreenState extends State<InstallerScreen> with WidgetsBindingOb
                       ],
                     ),
                   )
-              ],
-            ),
+                ],
+              ),
+            ],
           ),
         ),
       ),
-    );
-  }
+    ),
+  );
+}
 }
