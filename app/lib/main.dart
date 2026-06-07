556265820017a04236093dba5b4414a2208a34af
Tomaso Bulligan <tomaso.bulligan@gmail.com>
2026-06-07 11:53:23 +0200

    fix(app): correct syntax error in main.dart build scaffold

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1193,6 +1193,10 @@ class _InstallerScreenState extends State<InstallerScreen> with WidgetsBindingOb
                   )
                 ],
               ),
-  );
-}
+            ),
+          ),
+        ),
+      ),
+    );
+  }
 }
